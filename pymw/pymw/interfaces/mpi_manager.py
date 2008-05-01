c90b7c5a51444bcd6cee037978333a8d6169ef31
Eric Heien <eheien>
2008-05-01 08:34:03 +0000

    more changes for MPI

--- pymw/pymw/interfaces/mpi_manager.py
+++ pymw/pymw/interfaces/mpi_manager.py
@@ -3,26 +3,56 @@
 
 import mpi
 import os
+import time
+import threading
 
-finished = False
+def worker_control_thread(worker_num, task_sem, task_lock, task_list):
+	while True:
+		task_sem.acquire()
+		task_lock.acquire()
+		next_task = task_list.pop()
+		if not next_task:
+			task_lock.release()
+			return
+		task_lock.release()
+		mpi.send(next_task, worker_num)
+		mpi.recv(worker_num)
+		print next_task
 
-if mpi.rank == 0:
-	print mpi.size-1
+def master():
+	finished = False
+	task_list = []
+	task_sem = threading.Semaphore(0)
+	task_lock = threading.Lock()
+	control_threads = [threading.Thread(target=worker_control_thread, args=(worker_num, task_sem, task_lock, task_list)) for worker_num in range(1, mpi.size)]
+	for c_thread in control_threads:
+		c_thread.start()
+
 	while not finished:
 		try:
-			cmd = raw_input().split()
-			target = int(cmd[0])%(mpi.size-1)+1
-			msg = cmd[1]
-			mpi.send(msg, target)
+			cmd = raw_input()
+			task_lock.acquire()
+			task_list.append(cmd)
+			task_lock.release()
+			task_sem.release()
 		except:
 			for i in range(mpi.size):
 				mpi.send(None, i)
 			finished = True
-else:
+			for c_thread in control_threads:
+				task_list.append(None)
+				task_sem.release()
+
+def worker():
+	finished = False
 	while not finished:
 		msg, status = mpi.recv()
 		if msg is None:
 			finished = True
 		else:
-			print mpi.rank, msg
+			time.sleep(1)
+			mpi.send("booga", 0)
+
+if mpi.rank == 0: master()
+else: worker()
 
--- pymw/pymw/interfaces/score_interface.py
+++ pymw/pymw/interfaces/score_interface.py
@@ -1,34 +1,17 @@
 import subprocess
 import threading
-import cPickle
 import pymw
 import sys
 import os
 
-class _Worker:
-	def __init__(self, host_name):
-		self._active_task = None
-		self._host_name = host_name
-	
-class SCoreSystemInterface:
+class MPIInterface:
 	def __init__(self, num_workers=4, mpirun_loc="mpirun"):
-		# Use the scoregroups and scorehosts commands to find all hosts accessible from this machine
-		groups_check = subprocess.Popen(args=["scoregroups"],
-			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		group_list = groups_check.stdout.readline().split()
-		host_name_list = []
-		for group_name in group_list:
-			hosts_check = subprocess.Popen(args=["scorehosts", group_name],
-				stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			host_name_list.extend(hosts_check.stdout.readline().split())
-
 		# remove duplicate host names (for example, hosts in more than one group)
-		host_set = list(set(host_name_list))
-		self._worker_list = [_Worker(host) for host in host_set]
-		self._worker_list = self._worker_list[:num_workers]
-		self._count = 0
 		self._mpi_manager_process = subprocess.Popen(args=[mpirun_loc, "-np", str(num_workers+1), "/home/myri-fs/e-heien/local/bin/pyMPI", "/home/myri-fs/e-heien/osaka/pymw/pymw/pymw/interfaces/mpi_manager.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		task_finish_thread = threading.Thread(target=self._get_finished_tasks, args=(self))
+		task_finish_thread.start()
 		num_workers = int(self._mpi_manager_process.stdout.readline())
+		self.task_dict = {}
 	
 	def _save_state(self):
 		print "saving state"
@@ -37,20 +20,22 @@ class SCoreSystemInterface:
 		print "restoring state"
 	
 	def reserve_worker(self):
-		#self._count = (self._count+1)%len(self._worker_list)
-		self._count = self._count+1
-		return self._count
-		#return self._worker_list[self._count]
+		return None
 	
+	def _get_finished_tasks(self):
+		try:
+			while True:
+				task_name = self._mpi_manager_process.stdout.readline()
+				task = self.task_dict[task_name]
+				task.task_finished()
+		except:
+			pass
+
 	def execute_task(self, task, worker):
-		if sys.platform.startswith("win"):
-			print "SCore not yet supported on Windows"
-		else:
-			self._mpi_manager_process.stdin.write(str(worker)+str(task))
-			#task_process.wait()
-		
-		task.task_finished(pymw.pymw.InterfaceException("Boogaly"))
+		self.task_dict[str(task)] = task
+		self._mpi_manager_process.stdin.write(str(task))
 
 	def get_status(self):
 		return {}
+
 
