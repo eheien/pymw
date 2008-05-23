3365484b9a588d53e5147c789d996f547e4114d7
Eric Heien <eheien>
2008-05-23 07:19:36 +0000

    added default values for null master program

--- pymw/null_master.py
+++ pymw/null_master.py
@@ -6,14 +6,17 @@ import pymw.interfaces.multicore
 import pymw.interfaces.mpi
 import pymw.interfaces.boinc
 import time
-import logging
 from optparse import OptionParser
 
 parser = OptionParser(usage="usage: %prog")
-parser.add_option("-i", "--interface", dest="interface", help="specify the interface (multicore/mpi/boinc)", metavar="INTERFACE")
-parser.add_option("-n", "--num_workers", dest="num_w", help="number of workers", metavar="N")
-parser.add_option("-t", "--num_tasks", dest="num_t", help="number of tasks", metavar="N")
-parser.add_option("-p", "--project_home", dest="p_home", help="directory of the project (BOINC interface)", metavar="DIR")
+parser.add_option("-i", "--interface", dest="interface", default="multicore",
+				  help="specify the interface (multicore/mpi/boinc)", metavar="INTERFACE")
+parser.add_option("-n", "--num_workers", dest="num_w", default="4",
+				  help="number of workers", metavar="N")
+parser.add_option("-t", "--num_tasks", dest="num_t", default="10",
+				  help="number of tasks", metavar="N")
+parser.add_option("-p", "--project_home", dest="p_home", default="",
+				  help="directory of the project (BOINC interface)", metavar="DIR")
 options, args = parser.parse_args()
 
 num_w, num_t = int(options.num_w), int(options.num_t)
@@ -30,7 +33,7 @@ else:
 	print "Interface", options.interface, "unknown."
 	exit()
 
-pymw_master = pymw.pymw.PyMW_Master(interface=interface_obj, loglevel=logging.INFO)
+pymw_master = pymw.pymw.PyMW_Master(interface=interface_obj)
 
 post_init_time = time.time()
 tasks = [pymw_master.submit_task('null_worker.py', input_data=i) for i in range(num_t)]
