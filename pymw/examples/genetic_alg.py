a17c28a91fe04afb4bb142de05cf0b112a8abb15
Eric Heien <eheien>
2008-11-14 09:48:24 +0000

    updated ga code

--- pymw/examples/genetic_alg.py
+++ pymw/examples/genetic_alg.py
@@ -61,9 +61,15 @@ parser = OptionParser(usage="usage: %prog")
 parser.add_option("-i", "--interface", dest="interface", default="multicore", help="specify the interface (multicore/mpi/boinc)", metavar="INTERFACE")
 parser.add_option("-n", "--num_workers", dest="n_workers", default="4", help="number of workers", metavar="N")
 parser.add_option("-p", "--project_home", dest="p_home", default="", help="directory of the project (BOINC interface)", metavar="DIR")
+parser.add_option("-m", "--num_individuals", dest="n_inds", default="100", help="total number of individuals for genetic algorithm", metavar="DIR")
+parser.add_option("-l", "--gene_length", dest="g_len", default="100", help="gene length of each individual", metavar="DIR")
+parser.add_option("-s", "--sub_generations", dest="sub_gens", default="10", help="number of generations to run between gene exchanges", metavar="DIR")
+parser.add_option("-t", "--total_generations", dest="total_gens", default="500", help="total number of generations to run genetic algorithm", metavar="DIR")
+
 options, args = parser.parse_args()
 
-n_workers = int(options.n_workers)
+n_workers, num_inds, gene_len = int(options.n_workers), int(options.n_inds), int(options.g_len)
+sub_gens, total_gens = int(options.sub_gens), int(options.total_gens)
 
 start_time = time.time()
 
@@ -81,29 +87,25 @@ pymw_master = pymw.PyMW_Master(interface=interface_obj)
 
 post_init_time = time.time()
 
-num_inds = 20
-gene_len = 100
-num_gens = 10
 mut_rate = 1./gene_len
 cross_rate = 0.7
+inds_per_worker = num_inds
 
-gene_pool = [[random.randint(0,1) for i in range(gene_len)] for n in range(int(2*n_workers*num_inds))]
+gene_pool = [[random.randint(0,1) for i in range(gene_len)] for n in range(int(1.3*n_workers*num_inds))]
 
-num_active_tasks = 0
 max_fitness = 0
 
 for q in range(n_workers):
-    ind_set = random.sample(gene_pool, num_inds)
+    ind_set = random.sample(gene_pool, inds_per_worker)
     for x in ind_set: gene_pool.remove(x)
     pymw_master.submit_task(perform_ga, 
-                            input_data=(random.random(), mut_rate, num_gens, cross_rate, ind_set,),
+                            input_data=(random.random(), mut_rate, sub_gens, cross_rate, ind_set,),
                             modules=("random",),
                             dep_funcs=(crossover, select, mutate, fitness_func,))
     
-#while max_fitness < gene_len:
 gen_count = 0
-while gen_count < (1000*n_workers)/num_gens:
-    gen_count += 1
+while gen_count < total_gens:
+    gen_count += sub_gens
     old_max = max_fitness
     try:
         res_task, res = pymw_master.get_result()
@@ -112,19 +114,20 @@ while gen_count < (1000*n_workers)/num_gens:
     max_fitness = max(max_fitness, res[0])
     gene_pool.extend(res[1])
     
-    ind_set = random.sample(gene_pool, num_inds)
+    ind_set = random.sample(gene_pool, inds_per_worker)
     for x in ind_set: gene_pool.remove(x)
     pymw_master.submit_task(perform_ga, 
-                            input_data=(random.random(), mut_rate, num_gens, cross_rate, ind_set,),
+                            input_data=(random.random(), mut_rate, sub_gens, cross_rate, ind_set,),
                             modules=("random",),
                             dep_funcs=(crossover, select, mutate, fitness_func,))
 
 fitness = [fitness_func(ind) for ind in gene_pool]
 avg_fit = reduce(lambda x, y: x+y, fitness)/len(gene_pool)
-print max_fitness, avg_fit
 
 end_time = time.time()
 
+print "Best individual:", str(max_fitness),"/", str(gene_len)
+print "Average individual:", str(avg_fit),"/", str(gene_len)
 print "Number of workers:", str(n_workers)
 print "Calculation time:", str(end_time-start_time)
 print "Total time:", str(end_time-start_time)
