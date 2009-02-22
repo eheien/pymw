2b99d45739230ae689b523ba364508ec11eaa09e
Eric Heien <eheien>
2009-02-22 12:11:44 +0000

    tabs to space-tabs

--- pymw/pymw/interfaces/mpi.py
+++ pymw/pymw/interfaces/mpi.py
@@ -12,100 +12,99 @@ import logging
 import struct
 
 def send(pymw_socket, data):
-	size = struct.pack("!Q", len(data))
-	pymw_socket.sendall(size)
-	pymw_socket.sendall(data)
+    size = struct.pack("!Q", len(data))
+    pymw_socket.sendall(size)
+    pymw_socket.sendall(data)
 
 def recv(pymw_socket):
-	e_size = struct.calcsize("!Q")
-	r_size = 0
-	data = ""
-	while r_size < e_size:
-		msg = pymw_socket.recv(e_size-r_size)
-		if msg == "":
-			raise RuntimeError("Socket connection is broken")
-		r_size += len(msg)
-		data += msg
-	e_size = struct.unpack("!Q", data)[0]
+    e_size = struct.calcsize("!Q")
+    r_size = 0
+    data = ""
+    while r_size < e_size:
+        msg = pymw_socket.recv(e_size-r_size)
+        if msg == "":
+            raise RuntimeError("Socket connection is broken")
+        r_size += len(msg)
+        data += msg
+    e_size = struct.unpack("!Q", data)[0]
 
-	r_size = 0
-	data = ""
-	while r_size < e_size:
-		msg = pymw_socket.recv(e_size-r_size)
-		if msg == "":
-			raise RuntimeError("Socket connection is broken")
-		r_size += len(msg)
-		data += msg
-	return data
+    r_size = 0
+    data = ""
+    while r_size < e_size:
+        msg = pymw_socket.recv(e_size-r_size)
+        if msg == "":
+            raise RuntimeError("Socket connection is broken")
+        r_size += len(msg)
+        data += msg
+    return data
 
 class MPIInterface:
-	def __init__(self, num_workers=1, mpirun_loc="mpirun", startup_timeout=30, socket_port=43192):
-		if num_workers%2: num_workers -= 1    # to avoid crashes on our SCore cluster
+    def __init__(self, num_workers=1, mpirun_loc="mpirun", startup_timeout=30, socket_port=43192):
+        if num_workers%2: num_workers -= 1    # to avoid crashes on our SCore cluster
 
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.host = socket.gethostbyname(socket.gethostname())
-		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.socket.bind((self.host, socket_port))
-		self.socket.listen(1)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.host, socket_port))
+        self.socket.listen(1)
 
-		self._manager_process = subprocess.Popen(args=[mpirun_loc, "-np", str(num_workers),
-			"/home/myri-fs/e-heien/local/bin/pyMPI",
-			"/home/myri-fs/e-heien/osaka/pymw/pymw/interfaces/mpi_manager.py",
-			self.host, str(socket_port)],
-			stdin=None, stdout=None, stderr=None)
-			#stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		# accept connection from manager process
-		self.socket.settimeout(startup_timeout)
-		(csocket, address) = self.socket.accept()
-		self.csocket = csocket
+        self._manager_process = subprocess.Popen(args=[mpirun_loc, "-np", str(num_workers),
+            "/home/myri-fs/e-heien/local/bin/pyMPI",
+            "/home/myri-fs/e-heien/osaka/pymw/pymw/interfaces/mpi_manager.py",
+            self.host, str(socket_port)],
+            stdin=None, stdout=None, stderr=None)
+            #stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # accept connection from manager process
+        self.socket.settimeout(startup_timeout)
+        (csocket, address) = self.socket.accept()
+        self.csocket = csocket
 
-		self.csocket.setblocking(True)
-		#self.worker_count = recv(self.csocket)
-		self.task_dict = {}
-		self.mpi_error = None
-		task_finish_thread = threading.Thread(target=self._get_finished_tasks)
-		task_finish_thread.start()
-	
-	def reserve_worker(self):
-		return None
-	
-	def _get_finished_tasks(self):
-		try:
-			while True:
-				task_name = recv(self.csocket)
-				task = self.task_dict[task_name]
-				task.task_finished()
-		except KeyError, e:
-			self.mpi_error = e
-			return
-		except:
-			error_msg = self._manager_process.stderr.readlines()
-			full_msg = ""
-			for line in error_msg:
-				full_msg += line
-			self.mpi_error = Exception(full_msg)
-			return
+        self.csocket.setblocking(True)
+        #self.worker_count = recv(self.csocket)
+        self.task_dict = {}
+        self.mpi_error = None
+        task_finish_thread = threading.Thread(target=self._get_finished_tasks)
+        task_finish_thread.start()
+    
+    def reserve_worker(self):
+        return None
+    
+    def _get_finished_tasks(self):
+        try:
+            while True:
+                task_name = recv(self.csocket)
+                task = self.task_dict[task_name]
+                task.task_finished()
+        except KeyError, e:
+            self.mpi_error = e
+            return
+        except:
+            error_msg = self._manager_process.stderr.readlines()
+            full_msg = ""
+            for line in error_msg:
+                full_msg += line
+            self.mpi_error = Exception(full_msg)
+            return
 
-	def execute_task(self, task, worker):
-		if self.mpi_error:
-			task.task_finished(self.mpi_error)
-		#try:
-		self.task_dict[str(task)] = task
-		send(self.csocket, str(task)+" "+task._executable+" "+task._input_arg+" "+task._output_arg)
-		#except:
-		#	if self._manager_process.stderr:
-		#		error_msg = self._manager_process.stderr.readlines()
-		#	else:
-		#		error_msg = [""]
-		#	full_msg = ""
-		#	for line in error_msg:
-		#		full_msg += line
-		#	self.mpi_error = Exception(full_msg)
-		#	task.task_finished(self.mpi_error)
+    def execute_task(self, task, worker):
+        if self.mpi_error:
+            task.task_finished(self.mpi_error)
+        #try:
+        self.task_dict[str(task)] = task
+        send(self.csocket, str(task)+" "+task._executable+" "+task._input_arg+" "+task._output_arg)
+        #except:
+        #    if self._manager_process.stderr:
+        #        error_msg = self._manager_process.stderr.readlines()
+        #    else:
+        #        error_msg = [""]
+        #    full_msg = ""
+        #    for line in error_msg:
+        #        full_msg += line
+        #    self.mpi_error = Exception(full_msg)
+        #    task.task_finished(self.mpi_error)
 
-	def get_status(self):
-		return {}
+    def get_status(self):
+        return {}
 
-	def _cleanup(self):
-		self.csocket.close()
-
+    def _cleanup(self):
+        self.csocket.close()
--- pymw/pymw/interfaces/mpi_manager.py
+++ pymw/pymw/interfaces/mpi_manager.py
@@ -7,88 +7,87 @@ import struct
 import subprocess
 
 def send(comm_sock, data):
-	size = struct.pack("!Q", len(data))
-	comm_sock.sendall(size)
-	comm_sock.sendall(data)
+    size = struct.pack("!Q", len(data))
+    comm_sock.sendall(size)
+    comm_sock.sendall(data)
 
 def recv(comm_sock):
-	total_size = struct.calcsize("!Q")
-	recv_size = 0L
-	data = ""
-	try:
-		comm_sock.setblocking(0)
-		while recv_size < total_size:
-			msg = comm_sock.recv(total_size-recv_size)
-			if msg == "":
-				raise RuntimeError("Socket connection is broken")
-			recv_size += len(msg)
-			data += msg
-	except socket.error:
-		return None
-	finally:
-		comm_sock.setblocking(1)
+    total_size = struct.calcsize("!Q")
+    recv_size = 0L
+    data = ""
+    try:
+        comm_sock.setblocking(0)
+        while recv_size < total_size:
+            msg = comm_sock.recv(total_size-recv_size)
+            if msg == "":
+                raise RuntimeError("Socket connection is broken")
+            recv_size += len(msg)
+            data += msg
+    except socket.error:
+        return None
+    finally:
+        comm_sock.setblocking(1)
 
-	total_size = struct.unpack("!Q", data)[0]
+    total_size = struct.unpack("!Q", data)[0]
 
-	recv_size = 0L
-	data = ""
-	while recv_size < total_size:
-		msg = comm_sock.recv(total_size-recv_size)
-		if msg == "":
-			raise RuntimeError("Socket connection is broken")
-		recv_size += len(msg)
-		data += msg
-	return data
+    recv_size = 0L
+    data = ""
+    while recv_size < total_size:
+        msg = comm_sock.recv(total_size-recv_size)
+        if msg == "":
+            raise RuntimeError("Socket connection is broken")
+        recv_size += len(msg)
+        data += msg
+    return data
 
 def master():
-	try:
-		pymw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		pymw_socket.connect((sys.argv[1], int(sys.argv[2])))
-	except:
-		for worker_num in range(1, mpi.size):
-			mpi.send(None, worker_num)
-		raise
+    try:
+        pymw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        pymw_socket.connect((sys.argv[1], int(sys.argv[2])))
+    except:
+        for worker_num in range(1, mpi.size):
+            mpi.send(None, worker_num)
+        raise
 
-	#my_thread = threading.Thread(target=rank_0_worker, args=())
-	#my_thread.start()
+    #my_thread = threading.Thread(target=rank_0_worker, args=())
+    #my_thread.start()
 
-	worker_list = range(1, mpi.size)
-	#send(pymw_socket, str(mpi.size))
-	task_list = []
-	finish_notice_list = []
-	try:
-		while True:
-			cmd = recv(pymw_socket)
-			if cmd: task_list.append(cmd.split())
+    worker_list = range(1, mpi.size)
+    #send(pymw_socket, str(mpi.size))
+    task_list = []
+    finish_notice_list = []
+    try:
+        while True:
+            cmd = recv(pymw_socket)
+            if cmd: task_list.append(cmd.split())
 
-			# If we have a task and a worker available to do it
-			if len(task_list) > 0 and len(worker_list) > 0:
-				next_task = task_list.pop()
-				worker_num = worker_list.pop()
-				#print "Worker", str(worker_num), "sent task", str(next_task[0])
-				# Send the task to the worker
-				mpi.isend(next_task, worker_num)
-				notice_req = mpi.irecv(worker_num)
-				finish_notice_list.append([notice_req, worker_num, next_task])
-			for notice_req in finish_notice_list:
-				if notice_req[0]:
-					finished_task = notice_req[2]
-					#print "Worker", str(notice_req[1]), "finished task" , str(finished_task[0])
-					send(pymw_socket, finished_task[0])
-					worker_list.append(notice_req[1])
-					finish_notice_list.remove(notice_req)
+            # If we have a task and a worker available to do it
+            if len(task_list) > 0 and len(worker_list) > 0:
+                next_task = task_list.pop()
+                worker_num = worker_list.pop()
+                #print "Worker", str(worker_num), "sent task", str(next_task[0])
+                # Send the task to the worker
+                mpi.isend(next_task, worker_num)
+                notice_req = mpi.irecv(worker_num)
+                finish_notice_list.append([notice_req, worker_num, next_task])
+            for notice_req in finish_notice_list:
+                if notice_req[0]:
+                    finished_task = notice_req[2]
+                    #print "Worker", str(notice_req[1]), "finished task" , str(finished_task[0])
+                    send(pymw_socket, finished_task[0])
+                    worker_list.append(notice_req[1])
+                    finish_notice_list.remove(notice_req)
 
-	except RuntimeError:
-		for i in range(1, mpi.size):
-			mpi.send(None, i)
+    except RuntimeError:
+        for i in range(1, mpi.size):
+            mpi.send(None, i)
 
 def worker():
-	while True:
-		msg, status = mpi.recv()
-		if msg is None: return
-		retval = subprocess.call(["python", msg[1], msg[2], msg[3]])
-		mpi.send("done", 0)
+    while True:
+        msg, status = mpi.recv()
+        if msg is None: return
+        retval = subprocess.call(["python", msg[1], msg[2], msg[3]])
+        mpi.send("done", 0)
 
 if mpi.rank == 0: master()
 else: worker()
-
