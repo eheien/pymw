f4153098a6de7fea7c065cf09055d921d0f15635
Eric Heien <eheien>
2009-03-31 06:43:23 +0000

    Version update

--- pymw/setup.py
+++ pymw/setup.py
@@ -5,11 +5,11 @@ from distutils.core import setup
 DESC = """PyMW is a Python module for parallel master-worker computing in a variety of environments. With the PyMW module, users can write a single program that scales from multicore machines to global computing platforms."""
 
 setup(name='pymw',
-      version='0.2',
+      version='0.3',
       author='Eric Heien',
       author_email='e-heien@ist.osaka-u.ac.jp',
       url='http://pymw.sourceforge.net/',
-      description='Python Master Worker Computing',
+      description='Python Master-Worker Computing',
       long_description=DESC,
       license="MIT License",
       platforms=["any"],
