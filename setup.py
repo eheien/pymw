93c381c9220eb4909d9597dd7318bfc261dfe0f5
Eric Heien <github@heien.org>
2013-12-03 10:47:10 -0800

    Updated files for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 DESC = """PyMW is a Python module for parallel master-worker computing in a variety of environments. With the PyMW module, users can write a single program that scales from multicore machines to global computing platforms."""
 
 setup(name='pymw',
-      version='0.5',
+      version='0.4.1',
       author='Eric Heien',
       author_email='pymw@heien.org',
       url='http://pymw.sourceforge.net/',
